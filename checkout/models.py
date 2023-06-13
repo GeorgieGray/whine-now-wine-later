cfb6bec42a1d347a18b4fe3f0ae5c963013bc006
Georgie Gray <georginamirgray@gmail.com>
2023-06-14 00:33:07 +0100

    add purchase model

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from product.models import Product
 
-# Create your models here.
+class Purchase(models.Model):
+    PAYMENT_METHODS = [
+        ("C", "Credit Card"),
+        ("B", "Bitcoin")
+    ]
+    id = models.BigAutoField(primary_key=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    date = models.DateField()
+    paid = models.FloatField()
+    payment_method = models.CharField(max_length=1, choices=PAYMENT_METHODS)
