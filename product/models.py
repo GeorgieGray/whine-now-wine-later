91447d962309b20bfca9ea032507f42e9b8bdc7b
Georgie Gray <georginamirgray@gmail.com>
2023-06-14 23:07:35 +0100

    remove user model

--- product/models.py
+++ product/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-class User(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    first_name = models.CharField(max_length=32)
-    last_name= models.CharField(max_length=32)
-    email = models.EmailField(max_length=128)
-    password = models.CharField(max_length=32)
-
 class Product(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=32)
