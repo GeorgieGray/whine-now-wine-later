32a7cd6f9dae8125b8cb37367278e1cb95f74604
Georgie Gray <georginamirgray@gmail.com>
2023-06-16 14:29:45 +0100

    add stripe config endpoint

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Checkout
+from .views import Checkout, StripeConfig
 
 urlpatterns = [
+    path('config/', StripeConfig.as_view()),
     path('<id>/', Checkout.as_view(), name="checkout")
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 from product.models import Product
 from django.shortcuts import get_object_or_404
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import os
 
 class Checkout(TemplateView):
     template_name = "checkout.html"
@@ -11,3 +14,10 @@ class Checkout(TemplateView):
         self.product = get_object_or_404(Product, pk=id)
 
         return super(Checkout, self).dispatch(request, *args, **kwargs)
+
+class StripeConfig(View):
+
+    @csrf_exempt
+    def get(self, request):
+        public_key = os.getenv('STRIPE_PUBLISHABLE_KEY')
+        return JsonResponse({ 'publicKey': public_key }, safe=False)
