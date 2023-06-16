132d86d803091ec07d27a170f16507d783c62668
Georgie Gray <georginamirgray@gmail.com>
2023-06-16 16:18:28 +0100

    add buy via checkout

--- checkout/static/main.js
+++ checkout/static/main.js
@@ -1,7 +1,34 @@
-fetch("/checkout/api/config")
-    .then(result => result.json())
-    .then(({ publicKey }) => {
-        const stripe = Stripe(publicKey)
+const createCheckoutSession = () => {
+  return fetch(`/checkout/api/session/${window.productId}`, { method: 'POST' })
+  .then(result => {
+    if (!result.ok) throw new Error("Unable to create checkout session")
+    
+    return result.json()
+  })
+}
 
-        window._stripe = stripe
+const buyNow = async (stripe) => {
+  const { sessionId } = await createCheckoutSession()
+
+  console.log('session', sessionId)
+  
+  stripe.redirectToCheckout({ sessionId })
+}
+
+const init = async () => {
+  const { publicKey } = await fetch("/checkout/api/config")
+    .then(result => {
+      if (!result.ok) throw new Error("Unable to get checkout config")
+      
+      return result.json()
     })
+    
+  const stripe = Stripe(publicKey)
+  
+  const button = document.getElementById('buy-now')
+  if (!button) throw new Error("Cannot find buy now button")
+  
+  button.addEventListener("click", () => buyNow(stripe))
+}
+
+init()
