ae62ae5d5d495ee65dd130d5f09464f19b2435b1
Georgie Gray <georginamirgray@gmail.com>
2023-06-18 18:23:32 +0100

    add workout list, create and edit views

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from product.views import Buy
+from product.views import Buy, Workouts, CreateWorkout, EditWorkout
 
 urlpatterns = [
-    path('', Buy.as_view(), name="root")
+    path('', Buy.as_view(), name="root"),
+    path('workouts', Workouts.as_view(), name="workout_list"),
+    path('workouts/new', CreateWorkout.as_view(), name="create_workout"),
+    path('workouts/<id>', EditWorkout.as_view(), name="edit_workout")
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,18 @@
-from django.views.generic import ListView
-from .models import Product
+from django.views.generic import ListView, CreateView, UpdateView
+from .models import Product, Workout
 
 class Buy(ListView):
     template_name = "buy.html"
     model = Product
+
+class Workouts(ListView):
+    template_name = "workouts.html"
+    model = Workout
+
+class CreateWorkout(CreateView):
+    model = Workout
+    fields = ["type", "time", "date"]
+
+class EditWorkout(UpdateView):
+    model = Workout
+    fields = ["type", "time", "date"]
