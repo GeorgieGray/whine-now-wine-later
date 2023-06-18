90f44c9a98cb8fc50d668d024f6b9bb51dea1d65
Georgie Gray <georginamirgray@gmail.com>
2023-06-18 18:12:26 +0100

    remove redundant models

--- checkout/models.py
+++ checkout/models.py
@@ -1,13 +1,1 @@
 from django.db import models
-from product.models import Product
-
-class Purchase(models.Model):
-    PAYMENT_METHODS = [
-        ("C", "Credit Card"),
-        ("B", "Bitcoin")
-    ]
-    id = models.BigAutoField(primary_key=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    date = models.DateField()
-    paid = models.FloatField()
-    payment_method = models.CharField(max_length=1, choices=PAYMENT_METHODS)
--- user/models.py
+++ user/models.py
@@ -1,15 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import User
-from checkout.models import Purchase
-from product.models import Product
-
-class Membership(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE
-    )
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
-    start = models.DateField()
-    end = models.DateField()
