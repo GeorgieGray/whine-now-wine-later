a477690c1b6074b8cf403e552004eb2768ceffea
Georgie Gray <georginamirgray@gmail.com>
2023-06-18 19:10:27 +0100

    create and update workout views

--- product/urls.py
+++ product/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', Buy.as_view(), name="root"),
     path('workouts', Workouts.as_view(), name="workout_list"),
     path('workouts/new', CreateWorkout.as_view(), name="create_workout"),
-    path('workouts/<id>', EditWorkout.as_view(), name="edit_workout")
+    path('workouts/<pk>', EditWorkout.as_view(), name="edit_workout")
 ]
--- product/views.py
+++ product/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import ListView, CreateView, UpdateView
 from .models import Product, Workout
+from django.contrib.admin.widgets import AdminDateWidget
+from django import forms
+from django.contrib.auth import get_user_model
 
 class Buy(ListView):
     template_name = "buy.html"
@@ -10,9 +13,23 @@ class Workouts(ListView):
     model = Workout
 
 class CreateWorkout(CreateView):
+    template_name="workout_form.html"
     model = Workout
-    fields = ["type", "time", "date"]
+    fields = ["type", "time", "day"]
+    success_url="/workouts"
+
+    def get_form(self, form_class=None):
+        form = super(CreateWorkout, self).get_form(form_class)
+        form.fields['day'].widget = AdminDateWidget(attrs={'type': 'date'})
+        return form
+
+    def form_valid(self, form):
+        workout = form.save(commit=False)
+        workout.user = self.request.user
+        return super(CreateWorkout, self).form_valid(form)
 
 class EditWorkout(UpdateView):
+    template_name="workout_form.html"
     model = Workout
-    fields = ["type", "time", "date"]
+    fields = ["type", "time", "day"]
+    success_url="/workouts"
