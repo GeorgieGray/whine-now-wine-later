9f9313b876492ce7b995639370718f32961c58d3
Georgie Gray <georginamirgray@gmail.com>
2023-06-18 18:23:19 +0100

    add workout model

--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -13,3 +14,27 @@ class Product(models.Model):
 
     def __str__(self):
         return "Product: " + self.name
+
+class Workout(models.Model):
+    WORKOUT_TYPES = [
+        ("a", "Endurance"),
+        ("b", "Powerlifting"),
+        ("c", "Spin Indoor Cycling"),
+        ("d", "Flexibility")
+    ]
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    day = models.DateField(auto_now=False, auto_now_add=False)
+    time = models.TimeField(auto_now=False, auto_now_add=False)
+    type = models.CharField(max_length=1, choices=WORKOUT_TYPES)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user_id', 'day', 'time', 'type'],
+                name="one_class_per_slot_per_user"
+            )
+        ]
